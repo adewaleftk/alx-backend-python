db3e97931d2411ef6467806b40537c34dc771ede
Adewale Fatoki <adewaleftk@gmail.com>
2023-08-04 00:51:25 +0100

    python variable annotations

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Task 6'''
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    '''Adds up integers and floats and returns floats'''
+    return sum(mxd_lst)
