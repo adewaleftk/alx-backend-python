b776bfe87755c28d4b543740c90688590ac41a70
Adewale Fatoki <adewaleftk@gmail.com>
2023-08-04 00:04:42 +0100

    python variable annotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''Task 1'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''The concat concatenates two strings and returns a concatenated string'''
+    return str1 + str2
